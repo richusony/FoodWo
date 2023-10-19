3b08c006b062ccca3dc738a8feae990d484f3bf4
FoodWo Contributor <147939927+richu0001@users.noreply.github.com>
2023-10-19 12:10:09 +0000

    few changes

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -1,7 +1,9 @@
 const {couponModel} = require('../models/couponSchema')
+const productModel = require('../models/productSchema')
 
 async function viewCouponMangemenPage(req,res){
-    res.render('../views/Admin/couponMain.ejs');
+    const foodItem = await productModel.find({}).sort({productName:1})
+    res.render('../views/Admin/couponMain.ejs',{food:foodItem});
 }
 
 module.exports={
